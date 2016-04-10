2e598b329197e8bb4405964f6dfe2cdd347f7fc3
si.dawei <si.dawei@dd8dadaf-2b63-4768-808f-9c98a33fdf87>
2016-04-10 13:14:43 +0000

    重构 3.0版本

---
+++ baymax-spring/src/main/java/com/tongbanjie/baymax/spring/bean/ColumnConfig.java
@@ -0,0 +1,29 @@
+package com.tongbanjie.baymax.spring.bean;
+
+import com.tongbanjie.baymax.router.ColumnProcess;
+
+/**
+ * Created by sidawei on 16/4/10.
+ */
+public class ColumnConfig {
+
+    private String name;
+
+    private String process;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProcess() {
+        return process;
+    }
+
+    public void setProcess(String process) {
+        this.process = process;
+    }
+}
