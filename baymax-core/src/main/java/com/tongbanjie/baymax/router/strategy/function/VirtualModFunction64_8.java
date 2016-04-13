567ac3b2393c478116a8ccfb6c45ee1849951dd2
dong.peng <dong.peng@dd8dadaf-2b63-4768-808f-9c98a33fdf87>
2016-04-13 12:13:05 +0000

    64——8模式

---
+++ baymax-core/src/main/java/com/tongbanjie/baymax/router/strategy/function/VirtualModFunction64_8.java
@@ -0,0 +1,7 @@
+package com.tongbanjie.baymax.router.strategy.function;
+
+public class VirtualModFunction64_8 extends VirtualModFunction{
+	public VirtualModFunction64_8() {
+		super(64, 8);
+	}
+}
